60d5db853b78bd1b7ec2da33afe6fa8bae02b0ea
reneepc <rene.epcrdz@gmail.com>
2025-07-26 12:24:59 -0300

    Pass force flag

--- generate_repo.py
+++ generate_repo.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 from bootstrap import parse_args, load_talks, write_readme_file, download_pdf_files
 import sys
 
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     if not talks:
         print(f"No talks found in {args.json}")
-        if not args.force_empty:
+        if not args.force:
             if args.download_only:
                 print("Exiting without downloading PDFs.")
             else:
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     if args.readme_only:
         write_readme_file(talks, pdf_dir=args.out_dir)
     elif args.download_only:
-        download_pdf_files(talks, out_dir=args.out_dir)
+        download_pdf_files(talks, out_dir=args.out_dir, force=args.force)
     else:
         write_readme_file(talks, pdf_dir=args.out_dir)
-        download_pdf_files(talks, out_dir=args.out_dir)
+        download_pdf_files(talks, out_dir=args.out_dir, force=args.force)
